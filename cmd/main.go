4ff8f10bb66bec846a4d9011ef6a6d2652660022
andsala <5263089+andsala@users.noreply.github.com>
2017-12-10 17:10:49 +0100

    Change cli dep version

--- cmd/main.go
+++ cmd/main.go
@@ -5,8 +5,9 @@ import (
 
 	"fmt"
 	"github.com/andsala/geoip/ipdata"
-	"github.com/urfave/cli"
+	"gopkg.in/urfave/cli.v2"
 	"strings"
+	"time"
 )
 
 type Options struct {
@@ -18,30 +19,52 @@ type Options struct {
 var opt = Options{}
 
 func main() {
-	app := cli.NewApp()
+	app := &cli.App{}
 
 	app.Name = "geoip"
 	app.Usage = "Get info about IP geolocation"
 	app.Version = "0.0.1"
-	app.Author = "andsala"
+	app.Compiled = time.Now()
+	app.ArgsUsage = "[IP...]"
+	app.CustomAppHelpTemplate = `NAME:
+   {{.Name}} - {{.Usage}}{{if .Version}}
+VERSION:
+   {{.Version}}
+   {{end}}
+USAGE:
+   {{.HelpName}} {{if .VisibleFlags}}[options]{{end}} {{if .ArgsUsage}}{{.ArgsUsage}}{{else}}[arguments...]{{end}}
+   {{if len .Authors}}
+AUTHOR:
+   {{range .Authors}}{{ . }}{{end}}
+   {{end}}{{if .VisibleFlags}}
+GLOBAL OPTIONS:
+   {{range .VisibleFlags}}{{.}}
+   {{end}}{{end}}{{if .Copyright }}
+COPYRIGHT:
+   {{.Copyright}}
+   {{end}}
+`
 
 	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:        "api-key, a",
+		&cli.StringFlag{
+			Name:        "api-key",
+			Aliases:     []string{"a"},
 			Value:       "",
 			Usage:       "ipdata.co api key",
-			EnvVar:      "GEOIP_API_KEY",
+			EnvVars:     []string{"GEOIP_API_KEY"},
 			Destination: &opt.ApiKey,
 		},
-		cli.StringFlag{
-			Name:        "user-agent, u",
+		&cli.StringFlag{
+			Name:        "user-agent",
+			Aliases:     []string{"u"},
 			Value:       "",
 			Usage:       "HTTP user agent",
-			EnvVar:      "GEOIP_USER_AGENT",
+			EnvVars:     []string{"GEOIP_USER_AGENT"},
 			Destination: &opt.UserAgent,
 		},
-		cli.BoolFlag{
-			Name:        "ip-only, ip",
+		&cli.BoolFlag{
+			Name:        "ip-only",
+			Aliases:     []string{"ip"},
 			Usage:       "Print current public IP and exit",
 			Destination: &opt.IpOnly,
 		},
@@ -50,7 +73,7 @@ func main() {
 	app.Action = func(ctx *cli.Context) error {
 		client, err := ipdata.NewClient(nil)
 		if err != nil {
-			return cli.NewExitError(err, 2)
+			return cli.Exit(err, 2)
 		}
 
 		client.UserAgent = opt.UserAgent
@@ -61,7 +84,7 @@ func main() {
 		if opt.IpOnly {
 			data, err := client.GetMyIpData()
 			if err != nil {
-				return cli.NewExitError(err, 2)
+				return cli.Exit(err, 2)
 			}
 			fmt.Println(data.IP)
 			return nil
@@ -70,14 +93,14 @@ func main() {
 		if ctx.NArg() == 0 {
 			data, err := client.GetMyIpData()
 			if err != nil {
-				return cli.NewExitError(err, 2)
+				return cli.Exit(err, 2)
 			}
 			printIPData(*data)
 		} else {
-			for _, ip := range ctx.Args() {
+			for _, ip := range ctx.Args().Slice() {
 				data, err := client.GetIpData(ip)
 				if err != nil {
-					return cli.NewExitError(err, 2)
+					return cli.Exit(err, 2)
 				}
 				printIPData(*data)
 			}
